8348f4e47b10ced3f9732db638778c17d80564df
eli <elzr.sr@gmail.com>
2024-03-25 17:18:13 +0100

    Google badge and mockup with drop shadow

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			dropShadow: {
+        '3xl': '0 25px 25px rgba(0, 0, 0, 0.35)',
+      }
+		},
 	},
 	plugins: [],
 }
